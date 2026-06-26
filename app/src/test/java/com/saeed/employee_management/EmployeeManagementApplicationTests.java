6701ffab620ee2e3aefec297befceb529c9277fb
Saeed Gebaly <saeedgebaly7@gmail.com>
2026-06-27 01:53:53 +0300

    Configure H2 test profile

--- app/src/test/java/com/saeed/employee_management/EmployeeManagementApplicationTests.java
+++ app/src/test/java/com/saeed/employee_management/EmployeeManagementApplicationTests.java
@@ -2,12 +2,14 @@ package com.saeed.employee_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmployeeManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
